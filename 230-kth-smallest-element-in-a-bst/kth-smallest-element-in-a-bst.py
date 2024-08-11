0a66648a77d126239a376e2b2881eab46e4ea237
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-11 04:21:55 -0400

    Time: 57 ms (6.42%) | Memory: 19.4 MB (60.94%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -5,30 +5,42 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         # BRUTE-FORCE:
         #   go through every single node
         #   put them into a set
         #   sort the set
         #   return the k-th element
-        # first, go to the smallest node possible
-        # do a dfs and keep traversing left
-        def dfs(root):
-            if not root:
-                return
-            # print(root.val)
-            nodeSet.add(root.val)
-            # print(nodeSet)
-            if root.left:
-                dfs(root.left)
-            if root.right:
-                dfs(root.right)
-            return
-        nodeSet = set()
-        # print(nodeSet)
-        dfs(root)
-        # print(nodeSet)
-        nodeSet = sorted(nodeSet)
-        return nodeSet[k - 1]
+        
+        # BETTER SOLUTION: just do an in-order traversal
+        # stop once we have k elements in the in-order traversal
+        stack = []
+        curr = root
+        counter = 0
+        while curr or stack:
+            # add left children of the root
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            # pop element off stack
+            curr = stack.pop()
+            # process it
+            if (counter := counter + 1) == k:
+                return curr.val
+            curr = curr.right
+        return 0
+
+
+        # def dfs(root):
+        #     if not root:
+        #         return
+        #     nodeSet.add(root.val)
+        #     if root.left:
+        #         dfs(root.left)
+        #     if root.right:
+        #         dfs(root.right)
+        #     return
+        # nodeSet = set()
+        # dfs(root)
+        # nodeSet = sorted(nodeSet)
+        # return nodeSet[k - 1]
