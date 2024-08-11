e66dc97ec6c72f663e5ae0c85be51dd8f73e63c3
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-11 05:53:03 -0400

    Time: 641 ms (16.29%) | Memory: 23 MB (55.03%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,14 +1,5 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # easy - use minHeap
-        # heap will have k elements
-        # sorted by distance from origin
-        # if new point < last element in heap
-        # have a maxHeap instead
-        # heap will only ever have k elements at a time
-        # if newPoint < maxHeap.top():
-        #   heapq.heappush(newPoint)
-        # at the end, just return the k-element heap
         def euclid(x, y):
             # x2 and y2 are always 0 since we start at origin
             return x ** 2 + y ** 2
