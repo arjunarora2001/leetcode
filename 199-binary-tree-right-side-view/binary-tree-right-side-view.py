5c7cabfdb1b0918a81524dd59a716da5ba325277
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-11 01:54:30 -0400

    Time: 27 ms (96.91%) | Memory: 16.4 MB (94.95%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        # simple bfs
+        ans = []
+        if not root:
+            return ans
+        # queue = collections.deque([root])
+        queue = collections.deque()
+        queue.append(root)
+        # print(queue[-1].val)
+        # ans.append(root.val)
+        # simply return the last element of the queue at any given point
+        # after returning it, go through the level and push each element's children into the queue
+        while queue:
+            size = len(queue)
+            print(size)
+            ans.append(queue[-1].val) # we now have the right-most element
+            # curr = queue.popleft()
+            for i in range(size):
+                curr = queue.popleft()
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+                # queue.pop()
+        return ans
