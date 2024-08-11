89f757d5ade4a09b103532f4701492d460b51d71
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-10 21:45:24 -0400

    Time: 34 ms (89.70%) | Memory: 17.2 MB (68.13%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -6,17 +6,13 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # Start at root
-        # Push root on to the ans
-        # Push all of the children on to the queue (all left, right chilren)
         ans = []
         if not root:
             return ans
         queue = collections.deque([root])
         while queue:
-            size = len(queue)
             tmpStore = []
-            for i in range(size):
+            for i in range(len(queue)):
                 tmp = queue.popleft()
                 tmpStore.append(tmp.val)
                 if tmp.left:
@@ -25,29 +21,3 @@ class Solution:
                     queue.append(tmp.right)
             ans.append(tmpStore)
         return ans
-        # ans = []
-        # if not root:
-        #     return ans
-        # queue = deque([root])
-        # ans.append(root.val)
-        # if root.left:
-        #     queue.append(root.left)
-        # if root.right:
-        #     queue.append(root.right)
-        # # while queue not empty:
-        # while queue:
-        #     tmp = []
-        #     # first push every member of the queue (all children) into an array
-        #     for i in queue:
-        #         tmp.append(i.val)
-        #     # push that array into ans
-        #     ans.append(tmp)
-        #     # now pop all elements while extracting their left/right children and putting them into the queue
-        #     tmpQueue = queue
-        #     for i in tmpQueue:
-        #         if i.left:
-        #             queue.append(i.left)
-        #         if i.right:
-        #             queue.append(i.right)
-        #         queue.popleft()
-        # return ans
