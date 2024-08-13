3b726cee0c6ab41c9c48551441b5988050121922
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-13 02:16:08 -0400

    Time: 48 ms (40.00%) | Memory: 16.9 MB (80.82%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,17 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
         start = 0
         end = len(s) - 1
         while start < end:
-            # need to error check here to avoid seg fault
-            while not s[start].isalnum() and start < end:
-                start += 1
-            while not s[end].isalnum() and start < end:
-                end -= 1
-            if s[start] != s[end]:
-                return False
+            if s[start].isalnum():
+                if s[end].isalnum():
+                    if s[start].lower() != s[end].lower():
+                        return False
+                else:
+                    end -= 1
+                    continue
             else:
                 start += 1
-                end -= 1
+                continue
+            start += 1
+            end -= 1
         return True
