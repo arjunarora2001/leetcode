aeb0ada6888b8e802e676c92d97073770da9ddaf
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-15 02:18:14 -0400

    Time: 624 ms (40.89%) | Memory: 21.2 MB (15.58%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -6,23 +6,24 @@ class Solution:
             20: 0
         }
         for customer in bills:
-            if customer == 5:
-                register[5] += 1
-            elif customer == 10:
-                if register[5] == 0:
-                    return False
-                else:
-                    register[5] -= 1
-                    register[10] += 1
-            elif customer == 20:
-                # will have to change this
-                # the only time we'll need a 10 is with a 20
-                if register[10] >= 1 and register[5] >= 1:
-                    register[10] -= 1
-                    register[5] -= 1
-                elif register[10] == 0 and register[5] >= 3:
-                    register[5] -= 3
-                    register[20] += 1
-                else:
+            match customer:
+                case 5:
+                    register[5] += 1
+                case 10:
+                    if register[5] == 0:
+                        return False
+                    else:
+                        register[5] -= 1
+                        register[10] += 1
+                case 20:
+                    if register[10] >= 1 and register[5] >= 1:
+                        register[10] -= 1
+                        register[5] -= 1
+                    elif register[10] == 0 and register[5] >= 3:
+                        register[5] -= 3
+                        register[20] += 1
+                    else:
+                        return False
+                case _:
                     return False
         return True
