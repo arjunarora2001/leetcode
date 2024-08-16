69e457e2c590430e35fb34ba427cfbebe75f3713
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-16 06:06:55 -0400

    Time: 78 ms (30.46%) | Memory: 16.7 MB (19.66%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -1,11 +1,5 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        # d = dict(Counter(s))
-        # valSet = set(d.values())
-        # if len(valSet) != 1:
-        #     return False
-        # Not the end
-        # Order matters too
         size = len(s)
         divider = 2
         while divider <= size:
