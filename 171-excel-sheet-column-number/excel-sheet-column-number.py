4e68f8ba87f640d8afa5933b2ead374f29f8e04d
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-28 01:35:27 -0400

    Time: 41 ms (25.57%) | Memory: 16.4 MB (65.35%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.py
+++ 171-excel-sheet-column-number/excel-sheet-column-number.py
@@ -3,8 +3,6 @@ class Solution:
         digits = len(columnTitle)
         ans = 0
         for i in range(digits):
-            number = ord(columnTitle[i]) - ord('A') + 1
-            ans += (26 ** (digits - 1)) * number
-            # print(ans)
+            ans += (26 ** (digits - 1)) * (ord(columnTitle[i]) - ord('A') + 1)
             digits -= 1
         return ans
