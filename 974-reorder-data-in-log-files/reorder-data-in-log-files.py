6005d118736b8a8b307882eedd38108dc9e9735d
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-30 20:43:32 -0400

    Time: 44 ms (35.50%) | Memory: 16.8 MB (21.74%) - LeetSync

--- 974-reorder-data-in-log-files/reorder-data-in-log-files.py
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.py
@@ -1,11 +1,5 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        """
-        do a singular pass through logs and categorize them into digit logs and letter logs
-        have two hashMaps: one for digit logs and one for letter logs
-        find first space
-        if first character after space is a number, it's a digit log. else, it's a letter log.
-        """
         digits = []
         letters = []
         for log in logs:
@@ -17,21 +11,14 @@ class Solution:
                         digits.append(log)
                     else:
                         letters.append((contents, identifier))
-                        # letters[identifier] = contents
                     break
         # now we need to sort the letters map
-        # maybe use a minHeap for the letters contents?
-        # print(letters)
+        # use a minHeap for the letters contents
         heapq.heapify(letters)
-        # print(letters)
         ans = []
         while letters:
             elem = heapq.heappop(letters)
-            elemStr = ""
-            elemStr += elem[1]
-            elemStr += ' '
-            elemStr += elem[0]
-            ans.append(elemStr)
-        for i in digits:
-            ans.append(i)
+            ans.append(elem[1] + ' ' + elem[0])
+        # just add on digits since we need to maintain their relative ordering
+        ans.extend(digits)
         return ans
