c205447dde904f69eff217dec6bffe6cb1083eb1
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-31 02:31:16 -0400

    Time: 232 ms (79.30%) | Memory: 18.7 MB (95.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,20 +4,21 @@ class Solution:
         # keep searching until all land plots have been found
         # increment count from each point
         # dfs func must return a bool
-        seen = set()
+        # seen = set()
         islands = 0
         rows = len(grid)
         cols = len(grid[0])
 
         def isValid(row: int, col: int):
-            if row < 0 or row >= rows or col < 0 or col >= cols or (row, col) in seen or grid[row][col] != '1':
+            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] != '1':
                 return False
-            seen.add((row, col))
+            # seen.add((row, col))
             return True
 
         def dfs(row: int, col: int) -> bool:
             if not isValid(row, col):
                 return False
+            grid[row][col] = '0'
             # north
             dfs(row - 1, col)
             # east
@@ -32,7 +33,7 @@ class Solution:
 
         for row in range(rows):
             for col in range(cols):
-                if (row, col) not in seen and grid[row][col] == '1':
+                if grid[row][col] == '1':
                     if dfs(row, col):
                         islands += 1
         return islands
