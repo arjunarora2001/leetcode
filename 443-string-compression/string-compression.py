c997c763231ac2836c0f9a166f354076b5204056
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-30 22:41:56 -0400

    Time: 57 ms (63.83%) | Memory: 16.6 MB (52.42%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -2,16 +2,17 @@ class Solution:
     def compress(self, chars: List[str]) -> int:
         i = 0
         index = 0
-        while i < len(chars):
+        size = len(chars)
+        while i < size:
             char = chars[i]
             count = 0
-            while i < len(chars) and chars[i] == char:
+            while i < size and chars[i] == char:
                 i += 1
                 count += 1
             chars[index] = char
             index += 1
             if count >= 2:
-                for j in str(count):
-                    chars[index] = j
-                    index += 1
+                strCount = str(count)
+                chars[index : index + len(strCount)] = strCount
+                index += len(strCount)
         return index
