c9ce70702dbcbbdbf181888e4da146561fe57d1b
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-31 17:32:42 -0400

    Time: 562 ms (75.32%) | Memory: 34.4 MB (42.62%) - LeetSync

--- 2728-sum-in-a-matrix/sum-in-a-matrix.py
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.py
@@ -1,18 +1,27 @@
 class Solution:
     def matrixSum(self, nums: List[List[int]]) -> int:
+        rows = [sorted(row) for row in nums]
         score = 0
-        for operations in range(len(nums[0])):
-            maxVal = 0
-            for row in nums:
-                rowMax = row[0]
-                maxIndex = 0
-                for i in range(1, len(row)):
-                    if row[i] > rowMax:
-                        rowMax = row[i]
-                        maxIndex = i
-                row[maxIndex] = -1
-                if rowMax > maxVal:
-                    maxVal = rowMax
-            score += maxVal
+        for i in range(len(rows[0])):
+            rowMax = 0
+            for j in range(len(rows)):
+                if rows[j][i] > rowMax:
+                    rowMax = rows[j][i]
+            score += rowMax
         return score
+        # score = 0
+        # for operations in range(len(nums[0])):
+        #     maxVal = 0
+        #     for row in nums:
+        #         rowMax = row[0]
+        #         maxIndex = 0
+        #         for i in range(1, len(row)):
+        #             if row[i] > rowMax:
+        #                 rowMax = row[i]
+        #                 maxIndex = i
+        #         row[maxIndex] = -1
+        #         if rowMax > maxVal:
+        #             maxVal = rowMax
+        #     score += maxVal
+        # return score
             
