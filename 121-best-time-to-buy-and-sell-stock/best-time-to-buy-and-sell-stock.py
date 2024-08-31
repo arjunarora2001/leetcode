b4ba9db84d8f716274ad7d536bf3efdb6fcb597b
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-31 16:17:46 -0400

    Time: 776 ms (26.05%) | Memory: 27.3 MB (89.32%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -6,26 +6,3 @@ class Solution:
             lowestPrice = min(price, lowestPrice)
             maximum = max(maximum, price - lowestPrice)
         return maximum
-        # size = len(prices)
-        # j = 1
-        # i = 0
-        # while j < size and i < size - 1:
-        #     if prices[j] <= prices[i]:
-        #         j += 1
-        #         i += 1
-        #     else:
-        #         break
-        # if j == size:
-        #     return 0
-        # maximum = 0
-        # minVal = min(prices)
-        # for i in range(j - 1, size - 1):
-        #     localMax = max(prices[i + 1 : size]) - prices[i]
-        #     if localMax <= 0:
-        #         continue
-        #     else:
-        #         if prices[i] == minVal and localMax > maximum:
-        #             return localMax
-        #         else:
-        #             maximum = max(maximum, localMax)
-        # return maximum
