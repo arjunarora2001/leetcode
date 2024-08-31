356d4533c2786862e3125b83349d1cd35f29e795
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-31 17:44:11 -0400

    Time: 88 ms (61.64%) | Memory: 21.2 MB (45.38%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,9 +2,7 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         if k >= len(nums):
             return nums
-        freq = collections.defaultdict(int)
-        for num in nums:
-            freq[num] += 1
+        freq = Counter(nums)
         maxHeap = []
         for key, value in freq.items():
             heapq.heappush(maxHeap, (-value, key))
