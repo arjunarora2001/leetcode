6f8891f2ce2aa0069c8c2bc30dc3d9f67622a6be
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-31 02:26:18 -0400

    Time: 305 ms (17.11%) | Memory: 27.9 MB (9.86%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,39 +1,37 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        # do a DFS in all four directions from every point in the graph
+        # keep searching until all land plots have been found
+        # increment count from each point
+        # dfs func must return a bool
         seen = set()
-        numIslands = 0
+        islands = 0
         rows = len(grid)
         cols = len(grid[0])
-        def dfs(i, j):
-            if i < 0 or i >= rows or j < 0 or j >= cols:
+
+        def isValid(row: int, col: int):
+            if row < 0 or row >= rows or col < 0 or col >= cols or (row, col) in seen or grid[row][col] != '1':
                 return False
-            if (i, j) in seen:
+            seen.add((row, col))
+            return True
+
+        def dfs(row: int, col: int) -> bool:
+            if not isValid(row, col):
                 return False
-            # Process coordinate
-            seen.add((i, j))
-            # don't care if it's 0 (water)
-            if grid[i][j] == '0':
-                return False
-            # Process up
-            if i - 1 >= 0:
-                dfs(i - 1, j)
-            # Process right
-            if j + 1 < cols:
-                dfs(i, j + 1)
-            # Process down
-            if i + 1 < rows:
-                dfs(i + 1, j)
-            # Process left
-            if j - 1 >= 0:
-                dfs(i, j - 1)
-            if grid[i][j] == '1':
-                return True
-        for i in range(rows):
-            for j in range(cols):
-                if (i, j) not in seen and grid[i][j] == '1':
-                    if dfs(i, j):
-                        numIslands += 1
-        return numIslands
-            
+            # north
+            dfs(row - 1, col)
+            # east
+            dfs(row, col + 1)
+            # south
+            dfs(row + 1, col)
+            # west
+            dfs(row, col - 1)
+            return True
+
+
+
+        for row in range(rows):
+            for col in range(cols):
+                if dfs(row, col):
+                    islands += 1
+        return islands
