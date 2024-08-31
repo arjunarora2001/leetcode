24a5631b0b4fa2070d1b9ebf2fe7c86c11d3874a
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-08-31 16:21:16 -0400

    Time: 792 ms (14.15%) | Memory: 27.4 MB (89.32%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,31 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        lowestPrice = 10 ** 4
+        lowestPrice = prices[0]
         maximum = 0
-        for price in prices:
+        for price in prices[1:]:
             lowestPrice = min(price, lowestPrice)
             maximum = max(maximum, price - lowestPrice)
         return maximum
+        # size = len(prices)
+        # j = 1
+        # i = 0
+        # while j < size and i < size - 1:
+        #     if prices[j] <= prices[i]:
+        #         j += 1
+        #         i += 1
+        #     else:
+        #         break
+        # if j == size:
+        #     return 0
+        # maximum = 0
+        # minVal = min(prices)
+        # for i in range(j - 1, size - 1):
+        #     localMax = max(prices[i + 1 : size]) - prices[i]
+        #     if localMax <= 0:
+        #         continue
+        #     else:
+        #         if prices[i] == minVal and localMax > maximum:
+        #             return localMax
+        #         else:
+        #             maximum = max(maximum, localMax)
+        # return maximum
