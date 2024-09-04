e73c832af9158d4a5d36c3fa122c0d8de1873c40
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-09-04 14:47:56 -0400

    Time: 253 ms (85.45%) | Memory: 18.1 MB (59.55%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        maxPiles = max(piles)
+        if h == len(piles):
+            return maxPiles
+        """
+        sum(ceil(piles[i] / k)) == h
+        find max val in piles
+        calculate func for max val
+        keep calculating for max val - 1 until we get an answer where calculated h > h
+        """
+        # [1,...,maxPiles] -> answer lies somewhere here
+        minVal = maxPiles
+        # lower bound
+        left = 1
+        right = maxPiles
+        while left <= right:
+            mid = (left + right) // 2
+            hours = 0
+            for pile in piles:
+                hours += ceil(pile / mid)
+            if hours > h:
+                left = mid + 1
+            elif hours <= h:
+                minVal = min(minVal, mid)
+                right = mid - 1
+        return minVal
