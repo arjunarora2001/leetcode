390c651dedac706f2c74c6f89a1bf8fc785e621e
Arjun Arora <67394514+arjunarora2001@users.noreply.github.com>
2024-09-04 22:48:23 -0400

    Time: 51 ms (52.40%) | Memory: 16.5 MB (87.25%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,75 +1,47 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-        def validCell(row: int, col: int) -> bool:
-            if row < 0 or col < 0 or row >= rows or col >= cols:
-                return False
-            return True
-        # BFS:
-        q = collections.deque()
-        numFresh = 0
-        minutes = 0 
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 2:
-                    q.append([row, col])
-                elif grid[row][col] == 1:
-                    numFresh += 1
-        if not q and numFresh == 0:
-            return 0
-        if not q and numFresh > 0:
-            return -1
+        def isValid(row: int, col: int) -> bool:
+            return row >= 0 and row < len(grid) and col >= 0 and col < len(grid[0])
         minutes = 0
-        while q and numFresh > 0:
-            size = len(q)
-            for _ in range(size):
-                row, col = q.popleft()
-                for i, j in directions:
-                    if validCell(row + i, col + j):
-                        if grid[row + i][col + j] == 1:
-                            q.append([row + i, col + j])
-                            grid[row + i][col + j] = 2
-                            numFresh -= 1
+        queue = collections.deque()
+        numFresh = 0
+        numRotten = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
+                    numFresh += 1
+                elif grid[row][col] == 2:
+                    queue.append((row, col))
+        if numFresh == 0:
+            return 0
+        while queue:
             minutes += 1
-        return -1 if numFresh > 0 else minutes
-
-        # ITERATIVE: 
-
-        # have a boolean for whether anything has changed within a minute
-
-
-        # numFresh = 0
-        # changed = False
-        # rows = len(grid)
-        # cols = len(grid[0])
-        # minutes = 0
-        # for row in range(rows):
-        #     for col in range(cols):
-        #         if grid[row][col] == 1:
-        #             numFresh += 1
-        # if numFresh == 0:
-        #     return 0
-        # while True:
-        #     for row in range(rows):
-        #         for col in range(cols):
-        #             if grid[row][col] == 2:
-        #                 for i, j in directions:
-        #                     if validCell(row + i, col + j):
-        #                         if grid[row + i][col + j] == 1:
-        #                             grid[row + i][col + j] = -1
-        #                             numFresh -= 1
-        #                             if not changed:
-        #                                 changed = True
-        #     if not changed and numFresh > 0:
-        #         return -1
-        #     elif not changed:
-        #         return minutes
-        #     changed = False
-        #     for row in range(rows):
-        #         for col in range(cols):
-        #             if grid[row][col] == -1:
-        #                 grid[row][col] = 2
-        #     minutes += 1
-        # return minutes
+            # print(f"{minutes}, starting queue: {queue}")
+            tmp = queue.copy()
+            for _ in range(len(tmp)):
+                row, col = queue.popleft()
+                # up
+                if isValid(row - 1, col) and grid[row - 1][col] == 1:
+                    numFresh -= 1
+                    grid[row - 1][col] = 2
+                    queue.append((row - 1, col))
+                # right
+                if isValid(row, col + 1) and grid[row][col + 1] == 1:
+                    numFresh -= 1
+                    grid[row][col + 1] = 2
+                    queue.append((row, col + 1))
+                # down
+                if isValid(row + 1, col) and grid[row + 1][col] == 1:
+                    numFresh -= 1
+                    grid[row + 1][col] = 2
+                    queue.append((row + 1, col))
+                # left
+                if isValid(row, col - 1) and grid[row][col - 1] == 1:
+                    numFresh -= 1
+                    grid[row][col - 1] = 2
+                    queue.append((row, col - 1))
+            # print(f"{minutes}, ending queue: {queue}")
+        if numFresh != 0:
+            return -1
+        else:
+            return minutes - 1
